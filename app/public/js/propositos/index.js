3d81c55aaf8b7112bb9e1a9e6a413e89bee7c3d8
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2022-09-09 21:44:34 -0300

    arvore
    Co-authored-by: Caio Eduardo <CaioLr@users.noreply.github.com>

--- app/public/js/propositos/index.js
+++ app/public/js/propositos/index.js
@@ -2,3 +2,14 @@ async function apagar(id) {
   await $.ajax({ url: `http://localhost:3000/propositos/${id}`, method: 'DELETE'})
   location.reload()
 }
+
+async function buscar(el) {
+  const value = $("#busca" ).val();
+
+  if(value) {
+    window.location.replace(`http://localhost:3000/propositos?nome=${value}`);
+  } else {
+    window.location.replace(`http://localhost:3000/propositos`);
+  }
+
+}
